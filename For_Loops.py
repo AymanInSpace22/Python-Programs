dc38f22d114f262d6252d2581d51ea8e701f510b
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2021-02-12 22:53:14 -0600

    Create For_Loops.py

---
+++ For_Loops.py
@@ -0,0 +1,44 @@
+# For Loops
+
+# dont forget the colon
+# this just prints out each element in your list
+numbers = [1, 2, 3, 4, 5]
+for item in numbers:
+    print(item)
+
+print()
+
+# the while loop version
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
+
+print()
+
+# Range Function
+numbers = range(3, 20)
+for number in numbers:
+    print(number)
+
+print()
+
+# you can add the third value so it will count or skip by that number
+numbers = range(3, 20, 3)
+for number in numbers:
+    print(number)
+
+print()
+
+# another way of doing it
+for number in range(5)
+    print(number)
+
+
+
+# Tuples
+# similar to lists
+# with lists you use square brackets. With tuples you use paranthesis
+# tuples are immutable. Meaning you can not change them once they are done
+numbers = (1, 2, 3)
+numbers.
