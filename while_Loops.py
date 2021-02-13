dfbe58386383ecd915051156b100389c1cbe1890
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2021-02-12 22:50:57 -0600

    Create while_Loops.py

---
+++ while_Loops.py
@@ -0,0 +1,26 @@
+# while loops
+# p.s. python doesnt ues curly braces or semilcolons
+# p.p.s instead of curly braces, dont forget to use the colon
+
+
+# this is a simple while loop that counts to 5
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+print("")
+
+
+# you can muliply a number by a string, and it will repeat that string as much as the number gets incremented
+i = 1
+while i <= 10:
+    print(i * '*')
+    i += 1
+
+print()
+
+i = 1
+while i <+ 10:
+    print(i * "-_-")
+    i += 1
