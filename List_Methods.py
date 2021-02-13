1c46536d9c2f8d6b12a313a495a5513178197abe
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2021-02-12 22:51:45 -0600

    Create List_Methods.py

---
+++ List_Methods.py
@@ -0,0 +1,28 @@
+# List Methods
+
+
+# the append method lets you add a number to your list
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+
+# these methods let you add a number to the list, reverse the order of the list, and remove a number from the list
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+numbers.reverse()
+numbers.remove(2)
+print(numbers)
+
+# this lets you check if a subscript is present in your list
+numbers = [1, 2, 3, 4, 5]
+print(1 in numbers)
+
+# if you want to see how many items are present in a list. Use the bulit in len function
+numbers = [1, 2, 3, 4, 5]
+print(len(numbers))
+
+
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+numbers.remove(5)
+print(numbers)
